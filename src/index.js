bbd5862e487e0afa4f662e9844d53450afa88dd3
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-03-30 15:44:56 -0700

    updated code from lecture

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/Heroes.js
+++ src/pages/Heroes.js
@@ -1,16 +1,29 @@
 import { useState, useEffect } from 'react';
 import { heroData } from '../data/heroes';
-import { Link } from 'react-router-dom';
 import HeroCard from '../components/HeroCard'
 
 const Heroes = () => {
 
     const [heroes, setHeroes] = useState([]);
     const [alert, setAlert] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
-        setHeroes(heroData);
-    }, [])
+
+        // setHeroes(heroData);
+
+        const foundHeroes = heroData.filter(hd => {
+            return (
+                hd.superhero.toLowerCase().includes(searchTerm.toLowerCase()) +
+                hd.alter_ego.toLowerCase().includes(searchTerm.toLowerCase()) +
+                hd.publisher.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+        });
+
+        searchTerm === '' ? setHeroes(heroData) : setHeroes(foundHeroes);
+
+    }, [alert, searchTerm]);
 
     const updateFeatured = (heroId) => {
 
@@ -21,14 +34,21 @@ const Heroes = () => {
         setAlert(true);
         setTimeout(() => {
             setAlert(false);
-        });
+        }, 2000);
         
         console.log(foundHero);
 
     };
 
+    const handleChange = event => {
+
+        console.log(event.target.value);
+        setSearchTerm(event.target.value);
+
+    }
+
     return (
-        <div id="heroes">
+        <div id="Heroes">
 
             <div className="row text-center mt-3">
                 <div className="col">
@@ -37,13 +57,40 @@ const Heroes = () => {
             </div>
 
             <div className="row">
+                <div className="col">
+                    <div className="form=group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="hero-search"
+                            placeholder="Search for a Superhero"
+                            value={searchTerm}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    {alert ? (
+                        <div class="alert alert-success">
+                            Success
+                        </div>
+                    ) :
+                        ( <div></div> )
+                    }
+                </div>
+            </div>
+
+            <div className="row">
                     {heroes.map(hero => {
                         return (
-                        <div className="col-sm-12 col-md-3">
+                        <div className="col-sm-12 col-md-3" key={hero.id}>
                             <HeroCard hero={hero} updateFeatured={updateFeatured} />
                         </div>
-                        );
-                    })};
+                        )
+                    })}
             </div>
 
         </div>
